5407d41c50abd040e1c4e9b4a314b8a6f6711ad9
devloper-kartik <kartikmendu2@gmail.com>
2022-10-26 15:34:15 +0530

    first commit (Sign UP)

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './components/auth/Login';
+import SignUp from './components/auth/SignUp';
+
+function App() {
+  return (
+    <div className="App">
+      <SignUp/>
+    </div>
+  );
+}
+
+export default App;
